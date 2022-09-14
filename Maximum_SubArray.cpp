73a3ea06090cd9cedc1f5997fdd9caccee412300
Deep Patel <88723958+deeppatel51@users.noreply.github.com>
2022-09-14 12:27:58 +0530

    Create Maximum_SubArray.cpp

---
+++ Maximum_SubArray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int result = 0;
+        int findMax = INT_MIN;
+        
+        for(int i = 0; i < nums.size(); i++){
+            
+            result += nums[i];
+            
+            if(findMax < result){
+                findMax = result;
+            }
+            
+            if(result < 0){
+                result = 0;
+            }
+            
+        }
+        
+        return findMax;
+    }
+};
